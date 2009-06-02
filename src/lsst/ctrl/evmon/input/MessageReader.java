92ef629e48c84c47b0e23fa26350584c120b9498
srp <srp@git.lsstcorp.org>
2009-06-02 13:22:01 +0000

    missed the suffix of a comment block.  fixed.

--- src/lsst/ctrl/evmon/input/MessageReader.java
+++ src/lsst/ctrl/evmon/input/MessageReader.java
@@ -5,6 +5,7 @@ import lsst.ctrl.evmon.engine.MonitorMessage;
 /**
  * Interface MessageReader describes the interface used to retrieve messages
  * from input streams.
+ */
 public interface MessageReader {
     /**
      * Retrieve the next available message from the Reader's data source
